d0483bec363ff1229e9e08ab0a3eedc015ef3daa
Wolfgang Gerlach <wgerlach@users.noreply.github.com>
2021-01-29 10:46:22 -0600

    fix filename and permission

--- release.sh
+++ release.sh
@@ -59,6 +59,7 @@ cp -p deb/reverse/prerm ${BASEDIR}/DEBIAN/
 mkdir -p ${BASEDIR}/etc/systemd/system
 mkdir -p ${BASEDIR}/usr/bin
 cp -p ./waggle-reverse-tunnel.service ${BASEDIR}/etc/systemd/system/
-sed -e "s/{{VERSION}}/${VER_LONG}/; w ${BASEDIR}/usr/bin/waggle-reverse-tunnel.sh" ./waggle-reverse-tunnel.service
+sed -e "s/{{VERSION}}/${VER_LONG}/; w ${BASEDIR}/usr/bin/waggle-reverse-tunnel.sh" ./waggle-reverse-tunnel.sh
+chmod +x ${BASEDIR}/usr/bin/waggle-reverse-tunnel.sh
 
 dpkg-deb --root-owner-group --build ${BASEDIR} "${NAME}_${VER_SHORT}_${ARCH}.deb"
