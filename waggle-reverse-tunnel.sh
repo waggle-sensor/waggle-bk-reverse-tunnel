9bd24eb43c28850b5636dc4386cb05de5daf7c38
Wolfgang Gerlach <wgerlach@users.noreply.github.com>
2021-02-10 13:40:53 -0600

    fix ini parsing

--- waggle-reverse-tunnel.sh
+++ waggle-reverse-tunnel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 
-VERSION="{{VERSION}}"  # do not edit  
+VERSION="{{VERSION}}"  # do not edit
 
 
 CONFIG_FILE=/etc/waggle/config.ini
@@ -24,10 +24,10 @@ fi
 
 CONFIG_SECTION=$(grep '^\[registration\]' -A 999 ${CONFIG_FILE} | tail -n +2  | grep -B 999 '^\[' | head -n -1)
 
-BK_HOST=$(echo ${CONFIG_SECTION} | tr ' ' '\n' | grep host | cut -d '=' -f 2)
+BK_HOST=$(echo "${CONFIG_SECTION}" | grep host | cut -d '=' -f 2 |  tr -d ' ' )
 echo "BK_HOST=${BK_HOST}"
 
-BK_PORT=$(echo ${CONFIG_SECTION} | tr ' ' '\n' | grep port | cut -d '=' -f 2)
+BK_PORT=$(echo "${CONFIG_SECTION}" | grep port | cut -d '=' -f 2 |  tr -d ' ' )
 echo "BK_PORT=${BK_PORT}"
 
 # source: https://stackoverflow.com/a/64993893/2069181
@@ -38,6 +38,6 @@ set -x
 ssh -vv -N  -R /home_dirs/$id/rtun.sock:localhost:22 $id@${BK_HOST} -p ${BK_PORT} -i /etc/waggle/key.pem
 
 
-# -N Do not execute a remote command. This is useful for just forwarding ports (protocol version 2 only). 
+# -N Do not execute a remote command. This is useful for just forwarding ports (protocol version 2 only).
 # -R [bind_address:]port:host:hostport Specifies that the given port on the remote (server) host is to be forwarded to the given host and port on the local side.
 
