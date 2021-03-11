a9f2e6a39bf6b0aeab92d66e3aa226e97ced6b7a
Joe Swantek <joseph.swantek@northwestern.edu>
2021-03-11 17:16:43 -0600

    SAGE-752: get variables form 'reverse-tunnel' config section

--- waggle-reverse-tunnel.sh
+++ waggle-reverse-tunnel.sh
@@ -5,7 +5,6 @@ VERSION="{{VERSION}}"  # do not edit
 
 
 CONFIG_FILE=/etc/waggle/config.ini
-key_file=/etc/waggle/key.pem
 
 
 id=$(</etc/waggle/node-id)
@@ -22,7 +21,7 @@ if [ ! -e ${CONFIG_FILE} ] ; then
     exit 1
 fi
 
-CONFIG_SECTION=$(grep '^\[registration\]' -A 999 ${CONFIG_FILE} | tail -n +2  | grep -m1 -B 999 '^\[' | head -n -1)
+CONFIG_SECTION=$(grep '^\[reverse-tunnel\]' -A 999 ${CONFIG_FILE} | tail -n +2  | grep -m1 -B 999 '^\[' | head -n -1)
 
 BK_HOST=$(echo "${CONFIG_SECTION}" | grep host | cut -d '=' -f 2 |  tr -d ' ' )
 echo "BK_HOST=${BK_HOST}"
@@ -30,12 +29,15 @@ echo "BK_HOST=${BK_HOST}"
 BK_PORT=$(echo "${CONFIG_SECTION}" | grep port | cut -d '=' -f 2 |  tr -d ' ' )
 echo "BK_PORT=${BK_PORT}"
 
+KEY_FILE=$(echo "${CONFIG_SECTION}" | grep key | cut -d '=' -f 2 |  tr -d ' ' )
+echo "KEY_FILE=${KEY_FILE}"
+
 # source: https://stackoverflow.com/a/64993893/2069181
 
 
 set -x
 
-ssh -vv -N  -R /home_dirs/node-$id/rtun.sock:localhost:22 node-${id}@${BK_HOST} -p ${BK_PORT} -i ${key_file}
+ssh -vv -N  -R /home_dirs/node-$id/rtun.sock:localhost:22 node-${id}@${BK_HOST} -p ${BK_PORT} -i ${KEY_FILE}
 
 
 # -N Do not execute a remote command. This is useful for just forwarding ports (protocol version 2 only).
