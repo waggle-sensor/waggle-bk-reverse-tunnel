c0ff9b2d64fc84a3262458e6bb93c935a993139c
Sean Shahkarami <sean.shahkarami@gmail.com>
2021-03-18 15:25:09 -0500

    added support for ssh-options flag in waggle config. updated getconf to be more easily tested and added unit tests.

--- waggle-bk-reverse-tunnel.sh
+++ waggle-bk-reverse-tunnel.sh
@@ -1,41 +1,62 @@
 #!/bin/bash -e
 
+# getconf returns the first value for a key in a config file and section
+# TODO(sean) factor this out so this code and tests can share it
+getconf() {
+    file="$1"
+    sec="$2"
+    key="$3"
+
+    awk -v matchsec="$sec" -v matchkey="$key" '
+# skip comments
+/^#/ { next }
+
+# track current section
+/^\[.*\]/ {
+    # removes outer [ ]
+    gsub(/\[|\]/, "", $1);
+    sec = $1;
+}
+
+# print everything to after = when we match section and key 
+($2 == "=") && (sec == matchsec) && ($1 == matchkey) {
+    for (i = 3; i <= NF; i++) {
+        printf $i
+        if (i < NF) {
+            printf " "
+        }
+    }
+    exit
+}
+' "$file"
+}
 
 VERSION="{{VERSION}}"  # do not edit
 
-
 CONFIG_FILE=/etc/waggle/config.ini
 
-
 id=$(</etc/waggle/node-id)
-
 
 if [ "${id}_" == "_" ] ; then
     echo "Error: Node id missing"
     exit 1
 fi
 
-
 if [ ! -e ${CONFIG_FILE} ] ; then
     echo "Error: Config file ${CONFIG_FILE} missing"
     exit 1
 fi
 
-CONFIG_SECTION=$(grep '^\[reverse-tunnel\]' -A 999 ${CONFIG_FILE} | tail -n +2  | grep -m1 -B 999 '^\[' | head -n -1)
+BK_HOST=$(getconf ${CONFIG_FILE} reverse-tunnel host)
+BK_PORT=$(getconf ${CONFIG_FILE} reverse-tunnel port)
+KEY_FILE=$(getconf ${CONFIG_FILE} reverse-tunnel key)
+SSH_OPTIONS=$(getconf ${CONFIG_FILE} reverse-tunnel ssh-options)
+SSH_OPTIONS="${SSH_OPTIONS:-vv}"
 
-match=host
-BK_HOST=$(echo "${CONFIG_SECTION}" | grep -m1 ^[[:space:]]*${match}[[:space:]]*= | cut -d '=' -f 2 |  tr -d ' ' )
 echo "BK_HOST=${BK_HOST}"
-
-match=port
-BK_PORT=$(echo "${CONFIG_SECTION}" | grep -m1 ^[[:space:]]*${match}[[:space:]]*= | cut -d '=' -f 2 |  tr -d ' ' )
 echo "BK_PORT=${BK_PORT}"
-
-match=key
-KEY_FILE=$(echo "${CONFIG_SECTION}" | grep -m1 ^[[:space:]]*${match}[[:space:]]*= | cut -d '=' -f 2 |  tr -d ' ' )
 echo "KEY_FILE=${KEY_FILE}"
-
-# source: https://stackoverflow.com/a/64993893/2069181
+echo "SSH_OPTIONS=${SSH_OPTIONS}"
 
 set -x
 
@@ -51,3 +72,4 @@ ssh -vv \
 # -o "ServerAliveCountMax 3" Server can fail to respond to 3 pings before node closes connection.
 # -N Do not execute a remote command. This is useful for just forwarding ports (protocol version 2 only).
 # -R [bind_address:]port:host:hostport Specifies that the given port on the remote (server) host is to be forwarded to the given host and port on the local side.
+
